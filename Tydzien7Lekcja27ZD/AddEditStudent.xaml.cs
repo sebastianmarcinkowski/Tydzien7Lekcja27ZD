7dff3f0ed35259ca7b8b8307498bf06b395dfceb
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-09-17 13:13:55 +0200

    LEKCJA 8: Jak Poprawić Szybko Wygląd Aplikacji?

--- Tydzien7Lekcja27ZD/AddEditStudent.xaml.cs
+++ Tydzien7Lekcja27ZD/AddEditStudent.xaml.cs
@@ -1,23 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using MahApps.Metro.Controls;
 
 namespace Tydzien7Lekcja27ZD
 {
-    /// <summary>
-    /// Interaction logic for AddEditStudent.xaml
-    /// </summary>
-    public partial class AddEditStudent : Window
+    public partial class AddEditStudent : MetroWindow
     {
         public AddEditStudent()
         {
